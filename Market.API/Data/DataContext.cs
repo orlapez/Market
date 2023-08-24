d77077f8f9c920031ce8eaff1da48da22c6f6ddd
itmonuris <onurisitm@gmail.com>
2023-08-23 19:43:26 -0500

    Migración a sql server- Countries

---
+++ Market.API/Data/DataContext.cs
@@ -0,0 +1,29 @@
+﻿using Market.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Market.API.Data
+{
+    public class DataContext : DbContext
+    {
+
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+    }
+
+
+
+}
--- Market.API/Program.cs
+++ Market.API/Program.cs
@@ -1,3 +1,6 @@
+using Market.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// inyección de dependencias a SQL SERVER
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=DefaultConnection"));
+
+
 
 var app = builder.Build();
 
