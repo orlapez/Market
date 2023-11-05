d3af45ed5ed05fececcf2ef91049adc0b41b3359
orlapez <orlapez@gmail.com>
2023-11-05 14:55:02 -0500

    Controller owners

---
+++ Market.API/Controllers/OwnersController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Market.API.Data;
+using Market.Shared.Entities;
+
+namespace Veterinary.API.Controllers
+{
+
+    [ApiController]
+    [Route("/api/Owners")]
+    public class OwnersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public OwnersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        //Get con lista
+        //Select * From owners
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+
+            return Ok(await _context.Owners.ToListAsync());
+
+
+        }
+
+        // Get por parámetro
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult> Get(int id)
+        {
+
+            //200 Ok
+
+            var owner = await _context.Owners.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (owner == null)
+            {
+
+
+                return NotFound();
+            }
+
+            return Ok(owner);
+
+
+        }
+
+        // Crear un nuevo registro
+        [HttpPost]
+        public async Task<ActionResult> Post(Owner Owner)
+        {
+            _context.Add(Owner);
+            await _context.SaveChangesAsync();
+            return Ok(Owner);
+        }
+
+        // Actualizar o cambiar registro
+
+        [HttpPut]
+        public async Task<ActionResult> Put(Owner Owner)
+        {
+            _context.Update(Owner);
+            await _context.SaveChangesAsync();
+            return Ok(Owner);
+        }
+
+        // ELiminar registros
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+
+          
+            var FilaAfectada = await _context.Owners
+                .Where(x => x.Id == id)//5
+                .ExecuteDeleteAsync();  
+
+            if (FilaAfectada == 0)
+            {
+
+
+                return NotFound();//404
+            }
+
+            return NoContent();//204
+
+
+        }
+
+
+    }
+}
+
+
+
+
+
+
