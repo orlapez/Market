0f616ae699b2c9016a7c81922d778326ecc9fbcd
orlapez <orlapez@gmail.com>
2023-11-06 17:12:23 -0500

    V

---
+++ Market.API/Migrations/20231106221201_owners.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Market.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class owners : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Owners");
+
+            migrationBuilder.DropColumn(
+                name: "CellPhone",
+                table: "Owners");
+
+            migrationBuilder.DropColumn(
+                name: "Document",
+                table: "Owners");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Owners");
+
+            migrationBuilder.DropColumn(
+                name: "FixedPhone",
+                table: "Owners");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Owners");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Owners",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Owners");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Owners",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CellPhone",
+                table: "Owners",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Document",
+                table: "Owners",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Owners",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FixedPhone",
+                table: "Owners",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Owners",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
